d8cbc557e91731ddf920a07255ca246e08dfec50
ProrokWielki <ProrokWielki@o2.pl>
2022-08-15 16:07:51 +0200

    set client id to be by deafult the same as wifi hostname

--- config/mqtt_config.h
+++ config/mqtt_config.h
@@ -1,6 +1,8 @@
 #ifndef MQTT_CONFIG_H_
 #define MQTT_CONFIG_H_
 
+#include "wifi_config.h"
+
 namespace mqtt
 {
 constexpr auto server_ip = "255.0.0.0";
@@ -9,7 +11,7 @@ constexpr auto server_port = 1883;
 constexpr auto username = "MQTT Username";
 constexpr auto password = "MQTT Password";
 
-constexpr auto client_id = "ClientId";
+constexpr auto client_id = wifi::hostname;
 }  // namespace mqtt
 
 #endif /* MQTT_CONFIG_H_ */
